62979b78010624f68f393e4c0f978a6800ac1d79
Bikash Acharya <bikash.acharya614@gmail.com>
2024-10-25 22:51:17 +0545

    navbar linking li

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,7 +1,37 @@
+import { NavLink } from "react-router-dom";
 
-
-
-
-export default function Navbar(){
-    return <div>Navbar</div>
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center py-8 contailer max-auto flex-col lg:flex-row gap-5 lg:gap-0">
+      <h2 className="text-2xl font-semibold">
+        <NavLink to={"/Home"}>FoodRecipe</NavLink>
+      </h2>
+      <form>
+        <input
+          type="text"
+          name="search"
+          placeholder="Enter Items..."
+          className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200 "
+        />
+      </form>
+      <ul className="flex gap-5">
+        <li>
+          <NavLink
+            to={"/Home"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to={"/favorites"}
+            className="text-black hover:text-gray-700 duration-300"
+          >
+            favorites
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
