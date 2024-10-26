f10b082ef1f72d4c6f1a8b48eefa7595228fc9c9
Bikash Acharya <bikash.acharya614@gmail.com>
2024-10-26 23:19:25 +0545

    forn handling

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -1,16 +1,16 @@
 import { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import {GlobalContext} from "../../context";
+import { GlobalContext } from "../../context";
 
 export default function Navbar() {
-  const { searchParam, setSearchParam  } = useContext(GlobalContext);
+  const { searchParam, setSearchParam, handleSubmit  } = useContext(GlobalContext);
   console.log(searchParam);
   return (
     <nav className="flex justify-between items-center py-8 contailer max-auto flex-col lg:flex-row gap-5 lg:gap-0">
       <h2 className="text-2xl font-semibold">
         <NavLink to={"/Home"}>FoodRecipe</NavLink>
       </h2>
-      <form>
+      <form onSubmit ={handleSubmit}>
         <input
           type="text"
           name="search"
@@ -19,7 +19,7 @@ export default function Navbar() {
           placeholder="Enter Items..."
           className="bg-white/75 p-3 px-8 rounded-full outline-none lg:w-96 shadow-lg shadow-red-100 focus:shadow-red-200 "
         />
-      </form>
+      </form >
       <ul className="flex gap-5">
         <li>
           <NavLink
