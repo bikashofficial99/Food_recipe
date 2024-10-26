2f4f10ce5d56c429ebcb2996c386ea975f05f743
Bikash Acharya <bikash.acharya614@gmail.com>
2024-10-26 23:36:54 +0545

    fetching

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,6 +1,15 @@
+import { useContext } from "react"
+import { GlobalContext } from "../../context"
 
 
 
 export default function Home(){
-    return <div>Home</div>
+
+    const {} = useContext(GlobalContext);aa
+    return <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
+        {
+            recipeList && recipeList.length > 0 ?
+             : null
+        }
+    </div>
 }
