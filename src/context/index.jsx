90d4daf9d2251bbee7df426ac8d833596a917d60
Bikash Acharya <bikash.acharya614@gmail.com>
2024-10-26 23:20:12 +0545

    api data fetching

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -3,10 +3,23 @@ import { createContext, useState} from "react";
 export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
-  const [searchParam, setSearchParam] = useState("");
+  const [searchParam, setSearchParam ] = useState("");
 
+
+   async function handleSubmit(event){
+    event.preventDefault()
+    try {
+      const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
+      
+      );
+      const data = await res.json();
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+   }
   return (
-    <GlobalContext.Provider value={{ searchParam, setSearchParam }}>
+    <GlobalContext.Provider value={{ searchParam, setSearchParam, handleSubmit }}>
       {children}
     </GlobalContext.Provider>
   );
