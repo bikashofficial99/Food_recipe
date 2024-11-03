24e86324381a4212bf47e151987f7d8ede3da3d9
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-03 21:39:37 +0545

    home rend

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,23 +1,23 @@
-import { useContext } from "react"
-import { GlobalContext } from "../../context"
-import RecipeItem from "../../Components/Navbar/recipe-item";
-
-
+import { useContext } from "react";
+import { GlobalContext } from "../../context";
+import  RecipeItem  from "../../Components/recipe-item";
 
 export default function Home() {
+  const { recipeList, loading } = useContext(GlobalContext);
 
-    const { recipeList, loading } = useContext(GlobalContext);
+  if (loading) return <div>Loading....Please Wait!</div>;
 
-    if(loading) return <div>Loading....Please Wait!</div>;
-
-    return (
+  return (
     <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
-        { recipeList && recipeList.length > 0 
-        ? recipeList.map((item) => <RecipeItem item ={item} />)
-              : <div>
-                <p className="lg:text-4xl text-xl text-center text-black font-extrabold">Nothing to show. Please search something</p>
-                </div>}
-       
+      {recipeList && recipeList.length > 0 ? (
+        recipeList.map((item) => <RecipeItem item={item} />)
+      ) : (
+        <div>
+          <p className="lg:text-4xl text-xl text-center text-black font-extrabold">
+            Nothing to show. Please search something
+          </p>
+        </div>
+      )}
     </div>
-              );
+  );
 }
