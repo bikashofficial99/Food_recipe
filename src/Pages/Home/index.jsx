662970f9592535f3807f88b5e79e122d58ad0fec
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-04 14:11:58 +0545

    home sec

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
 import { GlobalContext } from "../../context";
-import  RecipeItem  from "../../Components/recipe-item";
+import RecipeItem from "../../Components/recipe-item";
 
 export default function Home() {
   const { recipeList, loading } = useContext(GlobalContext);
 
-  if (loading) return <div>Loading....Please Wait!</div>;
+  if (loading) return <div>Loading...Please wait!</div>;
 
   return (
     <div className="py-8 container mx-auto flex flex-wrap justify-center gap-10">
