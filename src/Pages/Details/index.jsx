a0346a3121f1fad1003f53abeb53797c715835cc
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-05 20:54:30 +0545

    extracting details id

--- src/Pages/Details/index.jsx
+++ src/Pages/Details/index.jsx
@@ -1,5 +1,23 @@
+import { useContext, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import { GlobalContext } from "../../context"
 
 
 export default function Details(){
+
+const {id} = useParams()
+const {recipeDetailsData, setRecipeDetailsData} = useContext(GlobalContext)
+
+useEffect(()=>{
+    async function getRecipeDetails() {
+        const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+    getRecipeDetails()
+},[])
+
+
     return <div>Details</div>
 }
