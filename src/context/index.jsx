0e81395191312a5db04e4b2060a565b38a9e8f6c
Bikash Acharya <bikash.acharya614@gmail.com>
2024-11-05 20:55:07 +0545

    passing param

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -6,7 +6,7 @@ export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState("");
   const [loading, setLoading] = useState(false);
   const [recipeList, setRecipeList] = useState([]);
-
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
 
    async function handleSubmit(event){
     event.preventDefault()
@@ -28,7 +28,7 @@ export default function GlobalState({ children }) {
   }
    console.log(loading, recipeList)
   return (
-    <GlobalContext.Provider value={{ searchParam,loading,recipeList, setSearchParam, handleSubmit }}>
+    <GlobalContext.Provider value={{ searchParam,loading,recipeList, setSearchParam, handleSubmit, recipeDetailsData, setRecipeDetailsData }}>
       {children}
     </GlobalContext.Provider>
   );
